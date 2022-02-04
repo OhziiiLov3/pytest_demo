c69d2ab5fab8be6ffb26a3ee7cf5b88a6d39b649
ohziiilov3 <klbaskerville0520@gmail.com>
2022-02-04 09:27:26 -0800

    pytest zero exception

--- tests/test_math.py
+++ tests/test_math.py
@@ -3,19 +3,40 @@ This module contains baisc unit tests for math operations.
 Thier purpose is to show how to use the pytest framework by example.
 
 """
+import pytest 
 
+# Python Uses Functions to evaluate Test vs Junit which uses classes 
 # A Basic Test Function, our test cas checks the basic operation of addition 
 # Pass test 
 
 def test_one_plus():
     # assert is bulit-in and evaluates as a boolean and raises an excpetion of type error
     assert 1 + 1 == 2 
+
 # Fail Test Case 
-
 # A Test Function to show Assertion Introspection
-
+# def test_one_plus_two():
+#     a = 1 
+#     b = 2
+#     c = 0
+#     assert a + b == c 
 def test_one_plus_two():
     a = 1 
     b = 2
-    c = 0
+    c = 3
     assert a + b == c 
+
+# Test case with an Exception 
+
+# Test Function that verifies an exception
+
+# def test_divide_by_zero():
+#     num = 1 / 0
+
+def test_divide_by_zero():
+    with pytest.raises(ZeroDivisionError) as e:
+        num = 1 / 0
+
+    assert 'division by zero' in str(e.value)
+
+
