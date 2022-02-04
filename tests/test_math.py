239243bfe283595325076e933f76fff55083a92e
ohziiilov3 <klbaskerville0520@gmail.com>
2022-02-04 09:16:02 -0800

    config assest test pass/fail

--- tests/test_math.py
+++ tests/test_math.py
@@ -4,8 +4,18 @@ Thier purpose is to show how to use the pytest framework by example.
 
 """
 
-# A Basic Test Function
+# A Basic Test Function, our test cas checks the basic operation of addition 
+# Pass test 
 
 def test_one_plus():
-    assert 1 + 1 ==2 
-    
+    # assert is bulit-in and evaluates as a boolean and raises an excpetion of type error
+    assert 1 + 1 == 2 
+# Fail Test Case 
+
+# A Test Function to show Assertion Introspection
+
+def test_one_plus_two():
+    a = 1 
+    b = 2
+    c = 0
+    assert a + b == c 
