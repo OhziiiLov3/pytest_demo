970a37cef8bafbfff44b4c6d78de551b1e7afbc0
ohziiilov3 <klbaskerville0520@gmail.com>
2022-02-04 10:07:23 -0800

    added parametrize test

--- tests/test_math.py
+++ tests/test_math.py
@@ -39,4 +39,38 @@ def test_divide_by_zero():
 
     assert 'division by zero' in str(e.value)
 
+# Parameterized Test Cases 
+# Mutliplication test idea 
+# Two postive intgers 
+# Identity: multiplying any number by 1 
+# Zero: multiplying any number by 0 
+# Positive by a negative 
+# Negative by a negative 
+# Multiply by floats 
+
+# def test_multiply_two_positive_ints():
+#     assert 2 * 3 == 6
+
+# def test_multiply_identity():
+#     assert 1 * 99 == 99 
+
+# def test_multiply_zero():
+#     assert 0 * 100 == 0 
+
+# DRY: Remember DOnt repeat yourself 
+
+products = [
+    (2, 3, 6),           # Positive Intgers 
+    (1, 99, 99),       # Identity
+    (0, 99, 0),        # Zero
+    (3, -4, -12),      # Positive by Negative 
+    (-5, -5, 25),      # Negative by Negative 
+    (2.5, 6.7, 16.75)  # Floats 
+]
+# Decorators is a special function that wraps around another function. The inner function is the test case 
+@pytest.mark.parametrize('a , b , product', products)
+def test_multiplication(a, b, product):
+    assert a * b == product
+
+
 
