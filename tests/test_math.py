51ca6c9f7b0505fefc0c2fe03c75a282f6ce9d3e
ohziiilov3 <klbaskerville0520@gmail.com>
2022-02-04 08:56:53 -0800

    pytest demo init repo

---
+++ tests/test_math.py
@@ -0,0 +1,11 @@
+""" 
+This module contains baisc unit tests for math operations.
+Thier purpose is to show how to use the pytest framework by example.
+
+"""
+
+# A Basic Test Function
+
+def test_one_plus():
+    assert 1 + 1 ==2 
+    
